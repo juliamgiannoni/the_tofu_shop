8800f1cb60f9c00ce1ff2f960ee7acd2e8dc4a60
Julia Yi <juliamijiyi@gmail.com>
2017-04-24 13:06:51 -0700

    Added Bootstrap styling, updated routes and added favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'customers/new'
 
-  get 'customers/create'
+  resources :customers, only: [:new, :create]
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
