616e650293781ef6d199c536be6d45ae44e41d1f
Julia Yi <juliamijiyi@gmail.com>
2017-04-24 16:11:21 -0700

    Updated current customer helper method and added authorize action before purchasing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
-    helper_method :current_user
+    helper_method :current_customer
 
-    def current_user
-      @current_user ||= Customer.find_by(id: session[:customer_id]) if session[:customer_id]
+    def current_customer
+      @current_customer ||= Customer.find_by(id: session[:customer_id]) if session[:customer_id]
+    end
+
+    def authorize
+      redirect_to login_path, alert: "Please login to complete your purchase" if current_customer.nil?
     end
 
 end
