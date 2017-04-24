ad49cbbf6a2247dc46a32b90b8d71e3464843064
Julia Yi <juliamijiyi@gmail.com>
2017-04-24 15:13:32 -0700

    Updated routes and added Carts, Categories, Products views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :customers, only: [:new, :create]
   resources :carts, only: [:index]
-  resources :products
-  resources :categories
+  resources :products, only: [:index, :show]
+  resources :categories, only: [:show]
 
 end
