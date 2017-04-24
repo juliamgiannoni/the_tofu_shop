874c6761ed6a911d49a000ae01d3a71d3a0a04f2
Julia Yi <juliamijiyi@gmail.com>
2017-04-24 12:36:53 -0700

    Created New and Create actions for Customer model

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,20 @@
 class CustomersController < ApplicationController
   def new
+    @customer = Customer.new
   end
 
   def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      flash[:notice] = "You've successfully created an account."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+private
+  def customer_params
+    params.require(:customer).permit(:customer_name, :customer_email, :password, :password_confirmation)
   end
 end
