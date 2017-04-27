b47a284b10631db48e20455e1134f8f73d347aa0
Julia Yi <juliamijiyi@gmail.com>
2017-04-26 17:47:39 -0700

    Updated shopping cart route

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -15,7 +15,7 @@ class CartsController < ApplicationController
   end
 
   def show
-    @cart = Cart.find(params[:id])
+    # @cart = Cart.find(params[:id])
     @products = Product.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
   root 'products#index'
+  get '/shoppingcart', to: 'carts#show'
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
+  get '/register', to: 'customers#new'
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :customers, only: [:new, :create]
   resources :carts, only: [:show]
   resources :products, only: [:index, :show]
 
-  get '/login', to: 'sessions#new'
-  get '/logout', to: 'sessions#destroy'
-  get '/register', to: 'customers#new'
+
 end
